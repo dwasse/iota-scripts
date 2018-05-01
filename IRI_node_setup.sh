ecf6ddaa1445cc5bcd06c4a58feb556103441ddd
Daniel Wasserman <wassermandaniel8@gmail.com>
2018-04-30 20:57:15 -0500

    Update IRI_node_setup.sh

--- IRI_node_setup.sh
+++ IRI_node_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Download required packages
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install software-properties-common -y && sudo add-apt-repository ppa:webupd8team/java -y && sudo apt update && sudo apt install oracle-java8-installer curl wget jq git -y && sudo apt install oracle-java8-set-default -y
